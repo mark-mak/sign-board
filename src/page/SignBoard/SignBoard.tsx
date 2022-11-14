18b2ad0146c87653efba545fe52032e973de5bba
mark-mak <markmak0000.dev@gmail.com>
2022-11-14 19:00:26 +0800

    feat: signBoard list, signPad option

--- src/page/SignBoard/SignBoard.tsx
+++ src/page/SignBoard/SignBoard.tsx
@@ -1,14 +1,148 @@
+import { useEffect, useState } from "react";
 import sign01 from "./demo/a.png";
+import sign02 from "./demo/b.png";
 import "./SignBoard.scss";
 
+const initialSignList = [
+  {
+    id: "id001",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id002",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id003",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id004",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id005",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id006",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id007",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id008",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id009",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id010",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id011",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id012",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id013",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id014",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id015",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id016",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id017",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id018",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id019",
+    src: sign01,
+    name: "sign01",
+  },
+  {
+    id: "id020",
+    src: sign01,
+    name: "sign01",
+  },
+];
+
+const initialCount = 20;
+
 const SignBoard: React.FC = () => {
+  const [signList, setSignList] = useState(initialSignList);
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log("useeffect");
+    const timer = setInterval(() => {
+      const result = [...signList];
+      const randomKey = Math.floor(Math.random() * 20);
+
+      result[randomKey] = {
+        id: "id".concat(`00${count + 1}`.slice(-3)),
+        src: result[randomKey].name === "sign01" ? sign02 : sign01,
+        name: result[randomKey].name === "sign01" ? "sign02" : "sign01",
+      };
+      setSignList(result);
+      setCount((prev) => prev + 1);
+
+      // console.log(result, randomKey);
+    }, 2000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [signList, count]);
+
   return (
     <div className="SignBoard">
       <div className="banner">
         <span>BANNER</span>
       </div>
       <div className="SignBoardContainer">
-        <img className="signImage" alt="sign" src={sign01} />
+        {signList.map((item) => (
+          <img key={item.id} className="signImage" alt="sign" src={item.src} />
+        ))}
       </div>
     </div>
   );
--- src/page/SignPad/SignPad.tsx
+++ src/page/SignPad/SignPad.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEventHandler, useState } from "react";
 import SignatureCanvas from "react-signature-canvas";
 
 import "./SignPad.scss";
@@ -6,12 +6,17 @@ import "./SignPad.scss";
 const SignPad: React.FC = () => {
   const [trimmedDataURL, setTrimmedDataURL] = useState<string>("");
   const [signPad, setSignPad] = useState<SignatureCanvas | null>(null);
+  const [penColor, setPenColor] = useState("black");
 
   const handleClear = () => {
     console.log(signPad);
     if (signPad) {
       signPad.clear();
     }
+  };
+
+  const handlePenColorChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPenColor(e.target.value);
   };
 
   const handleTrim = () => {
@@ -25,14 +30,59 @@ const SignPad: React.FC = () => {
     <div className="SignPad">
       <div className="signContainer">
         <SignatureCanvas
+          penColor={penColor}
           canvasProps={{ width: 500, height: 200 }}
           ref={(ref) => setSignPad(ref)}
         />
       </div>
-      <button className="" onClick={handleClear}>
+      <div>
+        <h4>Select Pen Color:</h4>
+        <div>
+          <input
+            type="radio"
+            id="penColorBlack"
+            name="penColor"
+            value="black"
+            onChange={handlePenColorChange}
+            defaultChecked
+          />
+          <label htmlFor="penColorBlack">black</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="penColorRed"
+            name="penColor"
+            value="darkRed"
+            onChange={handlePenColorChange}
+          />
+          <label htmlFor="penColorRed">red</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="penColorBlue"
+            name="penColor"
+            value="darkBlue"
+            onChange={handlePenColorChange}
+          />
+          <label htmlFor="penColorBlue">blue</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="penColorGreen"
+            name="penColor"
+            value="darkGreen"
+            onChange={handlePenColorChange}
+          />
+          <label htmlFor="penColorGreen">green</label>
+        </div>
+      </div>
+      <button className="actionButton" onClick={handleClear}>
         Clear
       </button>
-      <button className="" onClick={handleTrim}>
+      <button className="actionButton" onClick={handleTrim}>
         Trim
       </button>
       {trimmedDataURL ? (
